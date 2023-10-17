5d052c5b86ed9d93e284f110855c0c3cd9cc3eae
Zeynep Enkavi <zeynepenkavi@gmail.com>
2023-10-16 17:01:19 -0700

    outlining next steps as i read through module development

--- src/addm_grid_search.jl
+++ src/addm_grid_search.jl
@@ -37,8 +37,26 @@ include("util.jl")
 
 
 function aDDM_grid_search(addm::aDDM, fixationData::FixationData, dList::LinRange{Float64, Int64}, σList::LinRange{Float64, Int64},
-                          θList::LinRange{Float64, Int64}, n::Int64; trials::Int64=1000, cutOff::Int64=30000)
+                          θList::LinRange{Float64, Int64}, n::Int64; trials::Int64=1000, cutOff::Int64=30000, simData::Bool = false)
     """
+    Computes the likelihood of either observed or simulated data for all parameter combinations in paramGrid.
+      Args:
+        simData : boolean specifying whether to simulated data
+        expData : path to observed data if not simulating
+        fixationData : path to either observed fixation data if not simulating or output of `get_empirical_distributions` if simulating
+        paramGrid : grid of parameter combinations for which likelihoods of data will be computed
+        addm : addm object specifying the true model of simulating data
+        nTrials : number of trials to simulate for each dataset if simulating data
+        rtCutOff : max allowed RT in ms if simulating data 
+      Returns:
+        MLE estimates and likelihoods for 
+          
+    Todo:
+    - Specify how to read data in when not simulated
+    - Make parameter grid more general; not limited to only d, sigma and theta but also include bias, barrierDecay
+
+    aDDM_grid_search(simData::Bool = false, expData::..., fixationData::FixationData, paramGrid::..., # this is all you need if fitting to true data
+                    addm::aDDM, nTrials::Int64=1000, rtCutOff::Int64=30000) # additional args if simulating
     """
     dMLEList = Vector{Float64}(undef, n)
     σMLEList = Vector{Float64}(undef, n)
@@ -50,8 +68,13 @@ function aDDM_grid_search(addm::aDDM, fixationData::FixationData, dList::LinRang
     param_combinations = [(d, σ, θ) for d in dList, σ in σList, θ in θList]
     
     @showprogress for i in 1:n
-        addmTrials = aDDM_simulate_trial_data_threads(addm, fixationData, trials, cutOff=cutOff)
-        
+
+        if simData
+          addmTrials = aDDM_simulate_trial_data_threads(addm, fixationData, trials, cutOff = cutOff)
+        else
+          addmTrials = ...
+        end
+
         # Vectorized calculation of negative log-likelihood for all parameter combinations
         neg_log_like_array = [aDDM_negative_log_likelihood_threads(addm, addmTrials, d, σ, θ) for (d, σ, θ) in param_combinations]
         

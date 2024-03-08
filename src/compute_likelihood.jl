dcfa7493131a53b0b55ca49e54abb4ac151fbf52
Zeynep Enkavi <zeynepenkavi@gmail.com>
2024-03-07 17:13:56 -0800

    added multithreading and changed trial likelihood output to dict

--- src/compute_likelihood.jl
+++ src/compute_likelihood.jl
@@ -314,26 +314,43 @@ Compute likelihood of a dataset for a given model.
 - `likelihood_fn`: Name of the function that computes the likelhoods of a trial for the given model.
 - `likelihood_args`: Named tuple containing kwargs that should be fed to `likelihood_fn`
 - `return_trial_likelihoods`: Boolean to specify whether to return the likelihoods for each trial
+- `sequential_model`: Boolean to specify if the model requires all data concurrently (e.g. RL-DDM). If `true` model cannot be multithreaded
 
 # Returns
 - Negative log likelihood of data
+- (Optional) Dictionary of trial likelihoods keyed by the trial number
 """
-function compute_trials_nll(model::aDDM, data, likelihood_fn, likelihood_args = (timeStep = 10.0, approxStateStep = 0.1); 
-                            return_trial_likelihoods = false)
-  
-  # Todo: Add parallelization here?
-  likelihoods = [likelihood_fn(;model = model, trial = OneTrial, likelihood_args...) for OneTrial in data]
+function compute_trials_nll(model::ADDM.aDDM, data, likelihood_fn, likelihood_args = (timeStep = 10.0, approxStateStep = 0.1); 
+  return_trial_likelihoods = false, sequential_model = false)
+
+  n_trials = length(data)
+  likelihoods = Dict(zip(1:n_trials, zeros(n_trials)))
+  data_dict = Dict(zip(1:n_trials, data))
+
+  if sequential_model
+    for (trial_number, one_trial) in data_dict 
+      cur_lik = likelihood_fn(;model = model, trial = one_trial, likelihood_args...)
+      likelihoods[trial_number] = cur_lik
+    end
+  else
+    # Note using threads doesn't guaranteee likelihoods are returned in same order
+    # That's why the likelihoods are stored as key, value pairs so they can be rearranged later if needed
+    @threads for trial_number in collect(eachindex(data_dict))
+      one_trial = data_dict[trial_number]
+      cur_lik = likelihood_fn(;model = model, trial = one_trial, likelihood_args...)
+      likelihoods[trial_number] = cur_lik
+    end
+  end
 
   # If likelihood is 0, set it to 1e-64 to avoid taking the log of a 0.
-  likelihoods = max.(likelihoods, 1e-64)
+  likelihoods = Dict(k => max(v, 1e-64) for (k,v) in likelihoods)
 
   # Sum over all of the negative log likelihoods.
-  negative_log_likelihood = -sum(log.(likelihoods))
+  negative_log_likelihood = -sum(log.(values(likelihoods)))
 
   if return_trial_likelihoods
     return negative_log_likelihood, likelihoods
   else
     return negative_log_likelihood
   end
-
 end

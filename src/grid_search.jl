8726fc5dc6f06976687d4aa666305b858ef83a38
Zeynep Enkavi <zeynepenkavi@gmail.com>
2024-03-11 18:10:37 -0700

    updated to work with new outputs of compute_trials_nll. cleaned up output organization and docstring

--- src/grid_search.jl
+++ src/grid_search.jl
@@ -1,5 +1,19 @@
 """
-    grid_search(data, likelihood_fn, param_grid, return_grid_likelihoods = false; 
+    grid_search(data, param_grid, likelihood_fn = nothing, 
+                    fixed_params = Dict(:θ=>1.0, :η=>0.0, :barrier=>1, :decay=>0, :nonDecisionTime=>0, :bias=>0.0); 
+                    return_grid_nlls = true,
+                    likelihood_args = (timeStep = 10.0, approxStateStep = 0.1), 
+                    return_model_posteriors = false,
+                    model_priors = nothing,
+                    likelihood_fn_module = Main,
+                    sequential_model = false)
+                    fixed_params = Dict(:θ=>1.0, :η=>0.0, :barrier=>1, :decay=>0, :nonDecisionTime=>0, :bias=>0.0); 
+                    return_grid_nlls = true,
+                    likelihood_args = (timeStep = 10.0, approxStateStep = 0.1), 
+                    return_model_posteriors = false,
+                    model_priors = nothing,
+                    likelihood_fn_module = Main,
+                    sequential_model = false)
                 likelihood_args =  (timeStep = 10.0, approxStateStep = 0.1), return_trial_likelihoods = false)
 
 Compute the likelihood of either observed or simulated data for all parameter combinations in `param_grid`.
@@ -11,24 +25,25 @@ Compute the likelihood of either observed or simulated data for all parameter co
 - `data`: Data for which the sum of negative log likelihoods will be computed for each trial.
   Should be a vector of `ADDM.Trial` objects.
 - `likelihood_fn`: Name of likelihood function to be used to compute likelihoods. 
-  The toolbox has `ADDM.aDDM_get_trial_likelihood` and `ADDM.aDDM_get_trial_likelihood` defined.
+  The toolbox has `ADDM.aDDM_get_trial_likelihood` and `ADDM.DDM_get_trial_likelihood` defined.
   If comparing different generative processes then leave at default value of `nothing`
   and make sure to define a `likelihood_fn` in the `param_grid`.
-- `param_grid`: Grid of parameter combinations for which the sum of nll's for the `data` is 
-  computed.
+- `param_grid`: Parameter combinations for which the sum of nll's for the `data` is 
+  computed. Vector of NamedTuples.
 - `fixed_params`: Default `Dict(:θ=>1.0, :η=>0.0, :barrier=>1, :decay=>0, :nonDecisionTime=>0, :bias=>0.0)`.
   Parameters required by the `likelihood_fn` that are not specified to vary across likelihood 
   computations.
 
 ## Optional 
 
-- `return_grid_nlls`: Default `true`. If true, will return the sum of nll's for 
-  each parameter combination
-  in the grid search.
 - `likelihood_args`: Default `(timeStep = 10.0, approxStateStep = 0.1)`. Additional 
   arguments to be passed onto `likelihood_fn`. 
+- `return_grid_nlls`: Default `false`. If true, will return the sum of nll's for 
+  each parameter combination in the grid search.
 - `return_model_posteriors`: Default `false`. If true, will return the posterior probability 
   for each parameter combination in `param_grid`.
+- `return_trial_posteriors`: Default `false`. If true, will return the posterior probability 
+  for each parameter combination in `param_grid` after each trial in `data`.  
 - `model_priors`: priors for each model probability if not assummed to be uniform. Should be
   specified as a `Dict` with values of probabilities matching the keys for the correct model
   specified in `param_grid`.
@@ -36,16 +51,18 @@ Compute the likelihood of either observed or simulated data for all parameter co
 - `sequential_model`: Boolean to specify if the model requires all data concurrently (e.g. RL-DDM). If `true` model cannot be multithreaded
 
 # Returns
-- `best_part`: `Dict` containing the parameter combination with the lowest nll.
-- `all_nll_df`: `DataFrame` containing sum of nll's for each parameter combination.
-- `posteriors`: Likelihood for each trial for each parameter combination.
+- `best_pars`: `Dict` containing the parameter combination with the lowest nll.
+- `grid_nlls`: `DataFrame` containing sum of nll's for each parameter combination.
+- `trial_posteriors`: Posterior probability for each parameter combination after each trial.
+- `model_posteriors`: Posterior probability for each parameter combination after all trials.
 
 """
 function grid_search(data, param_grid, likelihood_fn = nothing, 
                     fixed_params = Dict(:θ=>1.0, :η=>0.0, :barrier=>1, :decay=>0, :nonDecisionTime=>0, :bias=>0.0); 
-                    return_grid_nlls = true,
                     likelihood_args = (timeStep = 10.0, approxStateStep = 0.1), 
+                    return_grid_nlls = false,
                     return_model_posteriors = false,
+                    return_trial_posteriors = false,
                     model_priors = nothing,
                     likelihood_fn_module = Main,
                     sequential_model = false)
@@ -64,7 +81,7 @@ function grid_search(data, param_grid, likelihood_fn = nothing,
   
   # Pass fixed parameters to the model
   # These don't need to be updated for each combination of the parameter grid
-  model = aDDM()
+  model = ADDM.aDDM()
   for (k,v) in fixed_params setproperty!(model, k, v) end
 
   # What should the structure of param_grid be?
@@ -106,98 +123,94 @@ function grid_search(data, param_grid, likelihood_fn = nothing,
     end
     
     # Make sure param names are converted to Greek symbols
-    convert_param_text_to_symbols!(model)
+    model = ADDM.convert_param_text_to_symbol(model)
 
     # !!!!!!!!! Add condition when sequential_model is false and threading should not be done
     if return_model_posteriors
       # Now trial likelihoods will be a dict indexed by trial numbers
-      all_nll[cur_grid_params], trial_likelihoods[cur_grid_params] = compute_trials_nll(model, data, likelihood_fn, likelihood_args; 
+      all_nll[cur_grid_params], trial_likelihoods[cur_grid_params] = ADDM.compute_trials_nll(model, data, likelihood_fn, likelihood_args; 
                               return_trial_likelihoods = true,  sequential_model = false)
 
     else
-      all_nll[cur_grid_params] = compute_trials_nll(model, data, likelihood_fn, likelihood_args)
+      all_nll[cur_grid_params] = ADDM.compute_trials_nll(model, data, likelihood_fn, likelihood_args)
     end
   
   end
 
-  # Wrangle likelihood data and extract best pars robustly before returning
+  # Wrangle likelihood data and extract best pars
 
-  # Extract best pars
-  # TODo: Convert param names to greek letters?
-  # !!!!!!!!! Confirm this argmin works when all_nll is a dictionary 
-  minIdx = argmin(all_nll)
-  best_fit_pars = Dict(pairs(param_grid[minIdx]))
-  best_pars = merge(best_fit_pars, fixed_params)
-  best_pars[:nll] = all_nll[minIdx]
+  # Extract best fit pars
+  best_fit_pars = argmin(all_nll)
+  best_pars = Dict(pairs(best_fit_pars))
+  best_pars = merge(best_pars, fixed_params)
+  best_pars = ADDM.convert_param_text_to_symbol(best_pars)
 
-  # !!!!!!!!! Fix return conditionals
+  # Begin collecting output
+  output = Dict()
+  output[:best_pars] = best_pars
+
   if return_grid_nlls
       # Add param info to all_nll
       all_nll_df = DataFrame()
-      for (k, v) in param_grid
-        row = DataFrame(Dict(pairs(v)))
-        row.nll .= all_nll[k]
-        # Appending won't work when different generative processes are part of param_grid
-        # append!(all_nll_df, row)
-        # vcat avoids that and can bind rows with different columns
+      for (k, v) in all_nll
+        row = DataFrame(Dict(pairs(k)))
+        row.nll .= v
+        # vcat avoids issues with append! and can bind rows with different columns
         all_nll_df = vcat(all_nll_df, row, cols=:union)
       end
-    
-    # Why is this conditional on return_grid_nlls?
-    # !!!!!!!!! Verify this all works with the new outputs of compute_trials_nll
-    if return_model_posteriors
-        
-        # Process trial likelihoods to compute model posteriors for each parameter combination
-        nTrials = length(data)
-        nModels = length(param_grid)
 
-        # Define posteriors as a dictionary with the same keys as param_grid
-        # Initialize the first posteriors as a float at 0 (value doesn't matter)
-        posteriors = Dict(zip(keys(param_grid), repeat([[0.0]], outer = nModels)))
-
-        if isnothing(model_priors)          
-          model_priors = Dict(zip(keys(param_grid), repeat([1/nModels], outer = nModels)))
-        end
-
-        # Trialwise posterior updating
-        for t in 1:nTrials
-
-          # Reset denominator p(data) for each trial
-          denom = 0
-
-          # Update denominator summing
-          for comb_key in keys(param_grid)
-            if t == 1
-              # Changed the initial posteriors so use model_priors for first trial
-              denom += (model_priors[comb_key] * trial_likelihoods[comb_key][t])
-            else
-              denom += (posteriors[comb_key][t-1] * trial_likelihoods[comb_key][t])
-            end
-          end
-
-          # Calculate the posteriors after this trial.
-          for comb_key in keys(param_grid)
-            if t == 1
-              prior = model_priors[comb_key]
-              # Assigning values to arrays in dictionaries is strange
-              # This changes the first value that was initialized at 0.0 to the updated posterior
-              # After trial 1
-              posteriors[comb_key] = [(trial_likelihoods[comb_key][t] * prior / denom)]
-            else
-              prior = posteriors[comb_key][t-1]
-              # After changing the first value we can now add the updated posteriors for the 
-              # following trials
-              append!(posteriors[comb_key],[(trial_likelihoods[comb_key][t] * prior / denom)])
-            end
-          end
-        end
-
-      return best_pars, all_nll_df, posteriors
-    else
-      return best_pars, all_nll_df
-    end
-  else
-    return best_pars
+      output[:grid_nlls] = all_nll_df
   end
+    
+  if return_model_posteriors
+      
+    # Process trial likelihoods to compute model posteriors for each parameter combination
+    nTrials = length(data)
+    nModels = length(param_grid)
+
+    # Define posteriors as a dictionary with models as keys and Dicts with trial numbers keys as values
+    trial_posteriors = Dict(k => Dict(zip(1:n_trials, zeros(n_trials))) for k in param_grid)
+
+    if isnothing(model_priors)          
+      model_priors = Dict(zip(keys(trial_likelihoods), repeat([1/nModels], outer = nModels)))
+    end
+
+    # Trialwise posterior updating
+    for t in 1:nTrials
+
+      # Reset denominator p(data) for each trial
+      denom = 0
+
+      # Update denominator summing
+      for comb_key in keys(trial_likelihoods)
+        if t == 1
+          # Changed the initial posteriors so use model_priors for first trial
+          denom += (model_priors[comb_key] * trial_likelihoods[comb_key][t])
+        else
+          denom += (trial_posteriors[comb_key][t-1] * trial_likelihoods[comb_key][t])
+        end
+      end
+
+      # Calculate the posteriors after this trial.
+      for comb_key in keys(trial_likelihoods)
+        if t == 1
+          prior = model_priors[comb_key]
+        else
+          prior = trial_posteriors[comb_key][t-1]
+        end
+        trial_posteriors[comb_key][t] = (trial_likelihoods[comb_key][t] * prior / denom)
+      end
+    end
+
+    if return_trial_posteriors
+      output[:trial_posteriors] = trial_posteriors
+    end
+
+    model_posteriors = Dict(k => trial_posteriors[k][nTrials] for k in keys(trial_posteriors))
+    output[:model_posteriors] = model_posteriors
+
+  end
+
+  return output
 
 end

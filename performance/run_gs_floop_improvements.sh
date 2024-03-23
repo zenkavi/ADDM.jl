5d805843773f0f717bc5c9261b52374a98c4c308
Zeynep Enkavi <zeynepenkavi@gmail.com>
2024-03-22 19:17:43 -0700

    debug job name

--- performance/run_gs_floop_improvements.sh
+++ performance/run_gs_floop_improvements.sh
@@ -3,6 +3,6 @@ for grid_search_exec in thread seq
 do
   for trials_exec in thread seq
   do
-    sed -e "s/{GRID_SEARCH_EXEC}/$grid_search_exec/g" -e "s/{TRIALS_EXEC}/$trials_exec/g" grid_search_benchmark_job.batch | sbatch
+    sed -e "s/{GRID_SEARCH_EXEC}/$grid_search_exec/g" -e "s/{TRIALS_EXEC}/$trials_exec/g" gs_floop_improvements_job.batch | sbatch
   done
 done

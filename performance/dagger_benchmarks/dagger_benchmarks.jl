c514cc220440e3c09c643c30d39ed18204475cd3
Zeynep Enkavi <zeynepenkavi@gmail.com>
2024-04-08 17:30:12 -0700

    refactor while prepping for dagger tests

--- performance/dagger_benchmarks/dagger_benchmarks.jl
+++ performance/dagger_benchmarks/dagger_benchmarks.jl
@@ -26,8 +26,8 @@ using Distributed
           help = "second positional argument; grid search function"
           arg_type = String
           required = true
-      "te"
-          help = "fourth positional argument; trials executor"
+      "tf"
+          help = "third positional argument; compute_trials function"
           arg_type = String
           required = true
   end
@@ -35,14 +35,14 @@ using Distributed
   parsed_args = parse_args(s)
   dp = parsed_args["dp"]
   gsf = parsed_args["gsf"]
-  te = parsed_args["te"]
+  ctf = parsed_args["tf"]
 
-  return dp, gsf, gse, te 
+  return dp, gsf, ctf 
 end
 
 println("Parsing arguments...")
 flush(stdout)
-dp, grid_search_fn, grid_search_exec, trials_exec = parse_commandline()
+dp, grid_search_fn, compute_trials_fn = parse_commandline()
 
 
 println("Defining functions...")
@@ -54,172 +54,38 @@ flush(stdout)
 #########################
 
 #########################
+# macro within BenchmarkTools for saving outputs
+#########################
+
+@eval BenchmarkTools macro btimed(args...)
+  _, params = prunekwargs(args...)
+  bench, trial, result = gensym(), gensym(), gensym()
+  trialmin, trialallocs = gensym(), gensym()
+  tune_phase = hasevals(params) ? :() : :($BenchmarkTools.tune!($bench))
+  return esc(quote
+      local $bench = $BenchmarkTools.@benchmarkable $(args...)
+      $BenchmarkTools.warmup($bench)
+      $tune_phase
+      local $trial, $result = $BenchmarkTools.run_result($bench)
+      local $trialmin = $BenchmarkTools.minimum($trial)
+      $result, $BenchmarkTools.time($trialmin), $BenchmarkTools.memory($trialmin)
+  end)
+end
+
+#########################
 # compute_trials_nll versions
 #########################
 
-@everywhere function compute_trials_nll_threads(model::ADDM.aDDM, data, likelihood_fn, likelihood_args = (timeStep = 10.0, approxStateStep = 0.1); 
-  return_trial_likelihoods = false, sequential_model = false)
-
-  n_trials = length(data)
-  likelihoods = Dict(zip(1:n_trials, zeros(n_trials)))
-  data_dict = Dict(zip(1:n_trials, data))
-
-  if sequential_model
-    for (trial_number, one_trial) in data_dict 
-      cur_lik = likelihood_fn(;model = model, trial = one_trial, likelihood_args...)
-      likelihoods[trial_number] = cur_lik
-    end
-  else
-    # Note using threads doesn't guaranteee likelihoods are returned in same order
-    # That's why the likelihoods are stored as key, value pairs so they can be rearranged later if needed
-    @threads for trial_number in collect(eachindex(data_dict))
-      one_trial = data_dict[trial_number]
-      cur_lik = likelihood_fn(;model = model, trial = one_trial, likelihood_args...)
-      likelihoods[trial_number] = cur_lik
-    end
-  end
-
-  # If likelihood is 0, set it to 1e-64 to avoid taking the log of a 0.
-  likelihoods = Dict(k => max(v, 1e-64) for (k,v) in likelihoods)
-
-  # Sum over all of the negative log likelihoods.
-  negative_log_likelihood = -sum(log.(values(likelihoods)))
-
-  if return_trial_likelihoods
-    return negative_log_likelihood, likelihoods
-  else
-    return negative_log_likelihood
-  end
-end
-
-@everywhere function compute_trials_nll_floop(model::ADDM.aDDM, data, likelihood_fn, likelihood_args = (timeStep = 10.0, approxStateStep = 0.1); 
-  return_trial_likelihoods = false, sequential_model = false, executor = ThreadedEx())
-
-  n_trials = length(data)
-  data_dict = Dict(zip(1:length(data), data))
-  likelihoods = Ref{Dict{Int64, Float64}}(Dict(zip(1:n_trials, zeros(n_trials))))
-
-  # Redundant but maybe more foolproof in case there is confusion about the executor
-  if sequential_model
-   cur_exe = SequentialEx()
-  else
-   cur_exe = executor
-  end
-
-  @floop cur_exe for trial_number in collect(eachindex(data_dict))
-    likelihoods[][trial_number] = likelihood_fn(;model = model, trial = data_dict[trial_number], likelihood_args...)
-  end
-
-  # If likelihood is 0, set it to 1e-64 to avoid taking the log of a 0.
-  likelihoods[] = Dict(k => max(v, 1e-64) for (k,v) in likelihoods[])
-
-  # Sum over all of the negative log likelihoods.
-  negative_log_likelihood = -sum(log.(values(likelihoods[])))
-
-  if return_trial_likelihoods
-    return negative_log_likelihood, likelihoods[]
-  else
-    return negative_log_likelihood
-  end
-end
-
-@everywhere function compute_trials_nll_dagger(model::ADDM.aDDM, data, likelihood_fn, likelihood_args = (timeStep = 10.0, approxStateStep = 0.1); 
-  return_trial_likelihoods = false, sequential_model = false)
-
-  n_trials = length(data)
-  # likelihoods = Dict(zip(1:n_trials, zeros(n_trials)))
-  likelihoods = DataFrame()
-  data_dict = Dict(zip(1:n_trials, data))
-
-  if sequential_model
-    for (trial_number, one_trial) in data_dict 
-      cur_lik = likelihood_fn(;model = model, trial = one_trial, likelihood_args...)
-      likelihoods[trial_number] = cur_lik
-    end
-  else
-    # Note using threads doesn't guaranteee likelihoods are returned in same order
-    # That's why the likelihoods are stored as key, value pairs so they can be rearranged later if needed
-    @sync for trial_number in collect(eachindex(data_dict))
-      one_trial = Dagger.@spawn data_dict[trial_number]
-      cur_lik = Dagger.@spawn likelihood_fn(;model = model, trial = one_trial, likelihood_args...)
-      # cur_lik = Dagger.@spawn likelihood_fn(;model = model, trial = one_trial)
-      # likelihoods[trial_number] = fetch.(cur_lik)
-      push!(likelihoods, (;trial_number, cur_lik))
-    end
-    likelihoods.cur_lik = fetch.(likelihoods.cur_lik)
-  end
-
-  # If likelihood is 0, set it to 1e-64 to avoid taking the log of a 0.
-  likelihoods.cur_lik = [max(v, 1e-64) for v in likelihoods.cur_lik]
-
-  # Sum over all of the negative log likelihoods.
-  negative_log_likelihood = -sum(log.(likelihoods.cur_lik))
-
-  if return_trial_likelihoods
-    return negative_log_likelihood, likelihoods
-  else
-    return negative_log_likelihood
-  end
-end
-
-@everywhere function compute_trials_nll_serial(model::ADDM.aDDM, data, likelihood_fn, likelihood_args = (timeStep = 10.0, approxStateStep = 0.1); 
-  return_trial_likelihoods = false, sequential_model = false)
-
-  n_trials = length(data)
-  likelihoods = Dict(zip(1:n_trials, zeros(n_trials)))
-  data_dict = Dict(zip(1:n_trials, data))
-
-  for (trial_number, one_trial) in data_dict 
-    cur_lik = likelihood_fn(;model = model, trial = one_trial, likelihood_args...)
-    likelihoods[trial_number] = cur_lik
-  end
-
-  # If likelihood is 0, set it to 1e-64 to avoid taking the log of a 0.
-  likelihoods = Dict(k => max(v, 1e-64) for (k,v) in likelihoods)
-
-  # Sum over all of the negative log likelihoods.
-  negative_log_likelihood = -sum(log.(values(likelihoods)))
-
-  if return_trial_likelihoods
-    return negative_log_likelihood, likelihoods
-  else
-    return negative_log_likelihood
-  end
-end
+@everywhere include("dagger_benchmarks_compute_trials.jl")
 
 #########################
 # grid_search versions
 #########################
 
-@everywhere function grid_search_thread_thread()
-
-@everywhere function grid_search_thread_floop()
-
-@everywhere function grid_search_thread_dagger()
-
-@everywhere function grid_search_thread_serial()
-
-@everywhere function grid_search_floop_thread()
-
-@everywhere function grid_search_floop_floop()
-
-@everywhere function grid_search_floop_dagger()
-
-@everywhere function grid_search_floop_serial()
-
-@everywhere function grid_search_dagger_thread()
-
-@everywhere function grid_search_dagger_floop()
-
-@everywhere function grid_search_dagger_dagger()
-
-@everywhere function grid_search_dagger_serial()
-
-@everywhere function grid_search_serial_thread()
-
-@everywhere function grid_search_serial_floop()
-
-@everywhere function grid_search_serial_dagger()
+@everywhere include("dagger_benchmarks_grid_search_thread.jl")
+@everywhere include("dagger_benchmarks_grid_search_floop.jl")
+@everywhere include("dagger_benchmarks_grid_search_dagger.jl")
+@everywhere include("dagger_benchmarks_grid_search_serial.jl")
     
 
 #########################
@@ -234,10 +100,11 @@ data = ADDM.load_data_from_csv(dp*"sim_data_beh.csv", dp*"sim_data_fix.csv");
 data = data["1"]; # Sim data is saved with parcode "1"
 
 # Read in parameter space
-# fn = dp*"/sim_data_grid_tst.csv"; #15000 combinations
-# fn = dp*"/sim_data_grid.csv";
+# fn = dp*"/sim_data_grid_tst.csv";
+# fn = dp*"/sim_data_grid.csv"; #15000 combinations
 fn = dp*"/sim_data_grid2.csv"; #8000 combinations
 tmp = DataFrame(CSV.File(fn, delim=","));
+tmp[!,"likelihood_fn"] .= "ADDM.aDDM_get_trial_likelihood";
 param_grid = NamedTuple.(eachrow(tmp));
 
 my_likelihood_args = (timeStep = 10.0, approxStateStep = 0.05);
@@ -250,7 +117,161 @@ fixed_params = Dict(:η=>0.0, :barrier=>1, :decay=>0, :nonDecisionTime=>100, :bi
 println("Selecting benchmark function...")
 flush(stdout)
 
-...
+# Case 1
+if grid_search_fn == "thread"
+  if compute_trials_fn == "thread"
+    f() = grid_search_thread_thread(data, param_grid, nothing, 
+    fixed_params, 
+    likelihood_args = my_likelihood_args, 
+    return_grid_nlls = false,
+    return_model_posteriors = true,
+    return_trial_posteriors = true)
+  end
+
+  if compute_trials_fn == "floop"
+    f() = grid_search_thread_floop(data, param_grid, nothing, 
+    fixed_params, 
+    likelihood_args = my_likelihood_args, 
+    return_grid_nlls = false,
+    return_model_posteriors = true,
+    return_trial_posteriors = true)
+  end
+
+  if compute_trials_fn == "dagger"
+    f() = grid_search_thread_dagger(data, param_grid, nothing, 
+    fixed_params, 
+    likelihood_args = my_likelihood_args, 
+    return_grid_nlls = false,
+    return_model_posteriors = true,
+    return_trial_posteriors = true)
+  end
+
+  if compute_trials_fn == "serial"
+    f() = grid_search_thread_serial(data, param_grid, nothing, 
+    fixed_params, 
+    likelihood_args = my_likelihood_args, 
+    return_grid_nlls = false,
+    return_model_posteriors = true,
+    return_trial_posteriors = true)
+  end
+end
+
+# Case 2
+if grid_search_fn == "floop"
+  if compute_trials_fn == "thread"
+    f() = grid_search_floop_thread(data, param_grid, nothing, 
+    fixed_params, 
+    likelihood_args = my_likelihood_args, 
+    return_grid_nlls = false,
+    return_model_posteriors = true,
+    return_trial_posteriors = true)
+  end
+
+  if compute_trials_fn == "floop"
+    f() = grid_search_floop_floop(data, param_grid, nothing, 
+    fixed_params, 
+    likelihood_args = my_likelihood_args, 
+    return_grid_nlls = false,
+    return_model_posteriors = true,
+    return_trial_posteriors = true)
+  end
+
+  if compute_trials_fn == "dagger"
+    f() = grid_search_floop_dagger(data, param_grid, nothing, 
+    fixed_params, 
+    likelihood_args = my_likelihood_args, 
+    return_grid_nlls = false,
+    return_model_posteriors = true,
+    return_trial_posteriors = true)
+  end
+
+  if compute_trials_fn == "serial"
+    f() = grid_search_floop_serial(data, param_grid, nothing, 
+    fixed_params, 
+    likelihood_args = my_likelihood_args, 
+    return_grid_nlls = false,
+    return_model_posteriors = true,
+    return_trial_posteriors = true)
+  end
+end
+
+# Case 3
+if grid_search_fn == "dagger"
+  if compute_trials_fn == "thread"
+    f() = grid_search_dagger_thread(data, param_grid, nothing, 
+    fixed_params, 
+    likelihood_args = my_likelihood_args, 
+    return_grid_nlls = false,
+    return_model_posteriors = true,
+    return_trial_posteriors = true)
+  end
+
+  if compute_trials_fn == "floop"
+    f() = grid_search_dagger_floop(data, param_grid, nothing, 
+    fixed_params, 
+    likelihood_args = my_likelihood_args, 
+    return_grid_nlls = false,
+    return_model_posteriors = true,
+    return_trial_posteriors = true)
+  end
+
+  if compute_trials_fn == "dagger"
+    f() = grid_search_dagger_dagger(data, param_grid, nothing, 
+    fixed_params, 
+    likelihood_args = my_likelihood_args, 
+    return_grid_nlls = false,
+    return_model_posteriors = true,
+    return_trial_posteriors = true)
+  end
+
+  if compute_trials_fn == "serial"
+    f() = grid_search_dagger_serial(data, param_grid, nothing, 
+    fixed_params, 
+    likelihood_args = my_likelihood_args, 
+    return_grid_nlls = false,
+    return_model_posteriors = true,
+    return_trial_posteriors = true)
+  end
+end
+
+# Case 4
+if grid_search_fn == "serial"
+  if compute_trials_fn == "thread"
+    f() = grid_search_serial_thread(data, param_grid, nothing, 
+    fixed_params, 
+    likelihood_args = my_likelihood_args, 
+    return_grid_nlls = false,
+    return_model_posteriors = true,
+    return_trial_posteriors = true)
+  end
+
+  if compute_trials_fn == "floop"
+    f() = grid_search_serial_floop(data, param_grid, nothing, 
+    fixed_params, 
+    likelihood_args = my_likelihood_args, 
+    return_grid_nlls = false,
+    return_model_posteriors = true,
+    return_trial_posteriors = true)
+  end
+
+  if compute_trials_fn == "dagger"
+    f() = grid_search_serial_dagger(data, param_grid, nothing, 
+    fixed_params, 
+    likelihood_args = my_likelihood_args, 
+    return_grid_nlls = false,
+    return_model_posteriors = true,
+    return_trial_posteriors = true)
+  end
+
+  if compute_trials_fn == "serial"
+    f() = grid_search_serial_serial(data, param_grid, nothing, 
+    fixed_params, 
+    likelihood_args = my_likelihood_args, 
+    return_grid_nlls = false,
+    return_model_posteriors = true,
+    return_trial_posteriors = true)
+  end
+end
 
 #########################
 # Run Benchmark
@@ -264,16 +285,12 @@ output, b_time, b_mem = BenchmarkTools.@btimed f()
 # Save outputs
 #########################
 
-println("Done benchmarking! Starting output processing...")
-flush(stdout)
-base_path = "outputs/grid_search_" * grid_search_fn * '_' * grid_search_exec * '_' * trials_exec * '_' * string(Dates.today()) * "_"
-
 println("Done benchmarking at " * string(now()))
 println("Starting output processing...")
 flush(stdout)
-base_path = "outputs/small_stepsize_" * grid_search_exec * '_' * string(now()) * "_"
+base_path = "outputs/dagger_benchmarks_" * grid_search_fn * '_' * compute_trials_fn * '_' * string(now()) * "_"
 
-b_time_df = DataFrame(:grid_search_fn => grid_search_fn, :grid_search_exec => grid_search_exec, :trials_exec => trials_exec, :b_time => b_time, :b_mem => b_mem)
+b_time_df = DataFrame(:grid_search_fn => grid_search_fn, :compute_trials_fn => compute_trials_fn, :b_time => b_time, :b_mem => b_mem)
 b_time_df[!, :nthreads] .= nthreads()
 b_time_path = base_path * "b_time.csv"
 CSV.write(b_time_path, b_time_df)
@@ -287,10 +304,12 @@ for (k,v) in output[:trial_posteriors]
 
   rename!(cur_df, :first => :trial_num, :second => :posterior)
 
+  # Unpack parameter info
   for (a, b) in pairs(k)
     cur_df[!, a] .= b
   end
 
+  # Change type of trial num col to sort by
   cur_df[!, :trial_num] = [parse(Int, (String(i))) for i in cur_df[!,:trial_num]]
 
   sort!(cur_df, :trial_num)
@@ -308,5 +327,4 @@ flush(stdout)
 # Usage
 #########################
 
-# julia --project=../ --threads 4 grid_search_benchmarks.jl /Users/zenkavi/Documents/RangelLab/aDDM-Toolbox/ADDM.jl/data/ floop2 thread thread 
-# julia --project=../ --threads 8 grid_search_benchmarks.jl /central/groups/rnl/zenkavi/ADDM.jl/data/ floop2 thread thread 
+# julia --project=../ --threads 4 dagger_benchmarks.jl /Users/zenkavi/Documents/RangelLab/aDDM-Toolbox/ADDM.jl/data/ thread dagger
---
+++ performance/dagger_benchmarks/dagger_benchmarks_compute_trials.jl
@@ -0,0 +1,133 @@
+@everywhere function compute_trials_nll_threads(model::ADDM.aDDM, data, likelihood_fn, likelihood_args = (timeStep = 10.0, approxStateStep = 0.1); 
+  return_trial_likelihoods = false, sequential_model = false)
+
+  n_trials = length(data)
+  likelihoods = Dict(zip(1:n_trials, zeros(n_trials)))
+  data_dict = Dict(zip(1:n_trials, data))
+
+  if sequential_model
+    for (trial_number, one_trial) in data_dict 
+      cur_lik = likelihood_fn(;model = model, trial = one_trial, likelihood_args...)
+      likelihoods[trial_number] = cur_lik
+    end
+  else
+    # Note using threads doesn't guaranteee likelihoods are returned in same order
+    # That's why the likelihoods are stored as key, value pairs so they can be rearranged later if needed
+    @threads for trial_number in collect(eachindex(data_dict))
+      one_trial = data_dict[trial_number]
+      cur_lik = likelihood_fn(;model = model, trial = one_trial, likelihood_args...)
+      likelihoods[trial_number] = cur_lik
+    end
+  end
+
+  # If likelihood is 0, set it to 1e-64 to avoid taking the log of a 0.
+  likelihoods = Dict(k => max(v, 1e-64) for (k,v) in likelihoods)
+
+  # Sum over all of the negative log likelihoods.
+  negative_log_likelihood = -sum(log.(values(likelihoods)))
+
+  if return_trial_likelihoods
+    return negative_log_likelihood, likelihoods
+  else
+    return negative_log_likelihood
+  end
+end
+
+@everywhere function compute_trials_nll_floop(model::ADDM.aDDM, data, likelihood_fn, likelihood_args = (timeStep = 10.0, approxStateStep = 0.1); 
+  return_trial_likelihoods = false, sequential_model = false, executor = ThreadedEx())
+
+  n_trials = length(data)
+  data_dict = Dict(zip(1:length(data), data))
+  likelihoods = Ref{Dict{Int64, Float64}}(Dict(zip(1:n_trials, zeros(n_trials))))
+
+  # Redundant but maybe more foolproof in case there is confusion about the executor
+  if sequential_model
+   cur_exe = SequentialEx()
+  else
+   cur_exe = executor
+  end
+
+  @floop cur_exe for trial_number in collect(eachindex(data_dict))
+    likelihoods[][trial_number] = likelihood_fn(;model = model, trial = data_dict[trial_number], likelihood_args...)
+  end
+
+  # If likelihood is 0, set it to 1e-64 to avoid taking the log of a 0.
+  likelihoods[] = Dict(k => max(v, 1e-64) for (k,v) in likelihoods[])
+
+  # Sum over all of the negative log likelihoods.
+  negative_log_likelihood = -sum(log.(values(likelihoods[])))
+
+  if return_trial_likelihoods
+    return negative_log_likelihood, likelihoods[]
+  else
+    return negative_log_likelihood
+  end
+end
+
+@everywhere function compute_trials_nll_dagger(model::ADDM.aDDM, data, likelihood_fn, likelihood_args = (timeStep = 10.0, approxStateStep = 0.1); 
+  return_trial_likelihoods = false, sequential_model = false)
+
+  n_trials = length(data)
+  # likelihoods = Dict(zip(1:n_trials, zeros(n_trials)))
+  likelihoods = DataFrame()
+  data_dict = Dict(zip(1:n_trials, data))
+
+  if sequential_model
+    for (trial_number, one_trial) in data_dict 
+      cur_lik = likelihood_fn(;model = model, trial = one_trial, likelihood_args...)
+      likelihoods[trial_number] = cur_lik
+    end
+  else
+    # Note using threads doesn't guaranteee likelihoods are returned in same order
+    # That's why the likelihoods are stored as key, value pairs so they can be rearranged later if needed
+    @sync for trial_number in collect(eachindex(data_dict))
+      one_trial = Dagger.@spawn data_dict[trial_number]
+      cur_lik = Dagger.@spawn likelihood_fn(;model = model, trial = one_trial, likelihood_args...)
+      # cur_lik = Dagger.@spawn likelihood_fn(;model = model, trial = one_trial)
+      # likelihoods[trial_number] = fetch.(cur_lik)
+      push!(likelihoods, (;trial_number, cur_lik))
+    end
+    likelihoods.cur_lik = fetch.(likelihoods.cur_lik)
+  end
+
+  # Convert likelihoods dataframe to dictionary for consistency with other compute_trials_nll functions
+  likelihoods_dict = Dict(zip(likelihoods.trial_number, likelihoods.cur_lik))
+
+  # If likelihood is 0, set it to 1e-64 to avoid taking the log of a 0.
+  # likelihoods.cur_lik = [max(v, 1e-64) for v in likelihoods.cur_lik]
+  likelihoods_dict = Dict(k => max(v, 1e-64) for (k,v) in likelihoods_dict)
+
+  # Sum over all of the negative log likelihoods.
+  negative_log_likelihood = -sum(log.(likelihoods.cur_lik))
+
+  if return_trial_likelihoods
+    return negative_log_likelihood, likelihoods
+  else
+    return negative_log_likelihood
+  end
+end
+
+@everywhere function compute_trials_nll_serial(model::ADDM.aDDM, data, likelihood_fn, likelihood_args = (timeStep = 10.0, approxStateStep = 0.1); 
+  return_trial_likelihoods = false, sequential_model = false)
+
+  n_trials = length(data)
+  likelihoods = Dict(zip(1:n_trials, zeros(n_trials)))
+  data_dict = Dict(zip(1:n_trials, data))
+
+  for (trial_number, one_trial) in data_dict 
+    cur_lik = likelihood_fn(;model = model, trial = one_trial, likelihood_args...)
+    likelihoods[trial_number] = cur_lik
+  end
+
+  # If likelihood is 0, set it to 1e-64 to avoid taking the log of a 0.
+  likelihoods = Dict(k => max(v, 1e-64) for (k,v) in likelihoods)
+
+  # Sum over all of the negative log likelihoods.
+  negative_log_likelihood = -sum(log.(values(likelihoods)))
+
+  if return_trial_likelihoods
+    return negative_log_likelihood, likelihoods
+  else
+    return negative_log_likelihood
+  end
+end

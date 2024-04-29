dc0bfd4dfe008f701a3566191ddd6ed36491d9ba
Zeynep Enkavi <zeynepenkavi@gmail.com>
2024-04-29 14:31:24 -0700

    updated docs oackages

--- docs/make.jl
+++ docs/make.jl
@@ -12,12 +12,13 @@ makedocs(
     pages = [
         "Home" => "index.md",
         "Tutorials" => ["tutorials/01_getting_started.md",
-                        "tutorials/02_likelihood_computation.md",
+                        #"tutorials/02_likelihood_computation.md",
                         "tutorials/03_empirical_data.md",
                         "tutorials/04_model_comparison.md",
-                        "tutorials/05_custom_model.md",
-                        "tutorials/06_parallelization.md",
-                        "tutorials/07_alt_optimization_algs.md"],
+                        #"tutorials/05_custom_model.md",
+                        #"tutorials/06_parallelization.md",
+                        #"tutorials/07_alt_optimization_algs.md"
+                        ],
         "API Reference" => "apireference.md",
     ],
     doctestfilters = [r"[\s\-]?\d\.\d{6}e[\+\-]\d{2}"],
